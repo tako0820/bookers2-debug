6bb7c4098aa41c5a70f702e9125b233239357928
tako0820 <105572635+tako0820@users.noreply.github.com>
2022-06-26 06:38:50 +0000

    [update]bookers2-debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :ensure_correct_user, only:[:edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :ensure_correct_user, only:[:edit, :update, :destroy]
+
 
   def show
     @user = User.find(params[:id])
