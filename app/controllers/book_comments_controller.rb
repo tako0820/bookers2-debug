8c7de4f0b474e53895c9138189b4e47ac8af8339
tako0820 <105572635+tako0820@users.noreply.github.com>
2022-07-04 09:12:33 +0000

    [update]コメント非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,12 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
   end
 
   private
