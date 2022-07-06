796e53fb17ca52d1c4f0a2aed92d73f211660a44
tako0820 <105572635+tako0820@users.noreply.github.com>
2022-07-06 10:38:59 +0000

    iine

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   before_action :authenticate_user!
   before_action :ensure_correct_user, only:[:edit, :update, :destroy]
+  before_action :ensure_guest_user, only: [:edit]
 
 
   def show
@@ -39,5 +40,12 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
+  
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end  
 
 end
